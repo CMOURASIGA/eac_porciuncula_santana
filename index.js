bdaf237dfa8ce413561ee79be233ebd0c562feba
CHRISTIAN MOURA DOS SANTOS <cmourasiga@gmail.com>
2025-05-07 20:44:33 -0300

    Update index.js
    token novo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-const token = 'EAAKOELSWQlIBO76Bvxp0LOVdo9ygq2cRndSz8BmBmagTAVJlKsOiBPGMs3KOSDPT7hVgOjJ1HYZBskzcXSvU09y6PdVSnKyZBYTm9TVIZC1Jbr2RIds6NdklrMyY5qh249aNjtM1o6wabMlNfwZC0EKdTzRjnHTQWbLyieMUtkQqRpdHb7ZBwS4KDxpuSqMonZAwugxYdWuARoZAwMHoH5Ro4QMWAgZD'; // Substitua aqui pelo token do WhatsApp Cloud API
+const token = 'EAAKOELSWQlIBOxBbw2XidvdMQ1Bks3emRTAyuD3eupRqY8qxDg02s5TfPfvUdZBcKHtTGsteGtZCUMZCkUdnNo4FNdax45WpSpqrszO7Gn1JAsZAGbo5zy0C75ZB7hEkNAM0GwSlrEtaSag7TulYPDmJEK5mIGnmUzUq62rY0ZBD0NnC0eQ1GIJPlZAQVX6J0rauy5kZAxZB69OibfrCJZBY6YZCozpLRkZD'; // Substitua aqui pelo token do WhatsApp Cloud API
 const phone_number_id = '572870979253681'; // Substitua aqui pelo ID do número de telefone
 const recipient_number = '5521998934970'; // Coloque o número de destino com DDI (ex: 5581...)
 
